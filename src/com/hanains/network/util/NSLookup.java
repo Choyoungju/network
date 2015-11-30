829ace419d505a735b1639788b8891c4d747d067
bit-user <bit-user@bit>
2015-11-30 13:40:33 +0900

    nslookup 주석으로 옵션 부여

--- src/com/hanains/network/util/NSLookup.java
+++ src/com/hanains/network/util/NSLookup.java
@@ -12,10 +12,10 @@ public class NSLookup {
 		Scanner scanner = new Scanner(System.in);
 		//scanner.nextLine();
 
-
+		
 
 		while(true){
-
+			InetAddress[] inetAddress = null;
 			System.out.println("\n도메인네임으로 입력해주세요");
 			String hostname = scanner.nextLine();
 
@@ -24,12 +24,19 @@ public class NSLookup {
 			}
 
 			try {
-				InetAddress[] inetAddress = InetAddress.getAllByName(hostname);
+				inetAddress = InetAddress.getAllByName(hostname);
 
 				for(int i=0; i<inetAddress.length; i++){
 					
-					System.out.print(inetAddress[i]); //양의 정스로 바꾸저ㅜㄹ 필요 (2의 보수 관련))
+					System.out.print(inetAddress[i].getHostAddress() + "\n"); //양의 정스로 바꾸저ㅜㄹ 필요 (2의 보수 관련))
 
+					
+					
+					//System.out.println(inetAddress[i].getHostName());
+					//System.out.println(inetAddress[i].getHostAddress());
+					//System.out.println(inetAddress[i].toString());  3가지 옵션이 있음.
+					
+					
 //					if(i+1<inetAddress.length){
 //						System.out.print(".");
 //					}
